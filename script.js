46a6a639b1a31c1e06ab5f0017b30ecafb1b0eed
zhaituona <zaytuna128@gmail.com>
2024-05-28 21:07:48 -0400

    homework07

---
+++ script.js
@@ -0,0 +1,30 @@
+// first declare couple of string varuables 
+const str1='You have recevied this message because you have been chosen to ';
+const str2 ='open an important vault.';
+const str3 ='Here is the secret combination:';
+console.log(str1 +" "+ str2+ " "+ str3);
+
+ // Let us assume combination lock code is made up of the numbers 5, 10, 20. 
+// Declare Variable a 
+const a =2+3;  
+console.log(a);
+
+//declare variable b
+const b =20-10;
+console.log(b);
+
+//declare variable c
+const c =4*5;
+console.log(c);
+
+
+
+
+
+
+  const wholeMessage = str1 +" " + str2+ " " + str3 ;
+  const value = a +b +c;
+
+
+
+alert(wholeMessage + value);
